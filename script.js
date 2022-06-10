be9a482a571cf9e1eea1f5592e9c453dd277aa4b
Jainam <91124384+jainam03@users.noreply.github.com>
2022-06-10 10:44:56 +0530

    .

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"
+];
+
+let buttonEl = document.getElementById("button")
+let fieldEl = document.getElementById("field")
+
+buttonEl.addEventListener("click", function () {
+    let randomPass1 = 15 + (Math.floor(Math.random() * characters.length))
+
+    let randomPass2 = 15 + (Math.floor(Math.random() * characters.length))
+
+    fieldEl.textContent = randomPass1
+})
