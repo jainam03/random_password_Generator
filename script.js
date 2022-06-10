72bcfbf59148a25a137463356557a624fb052931
Jainam <91124384+jainam03@users.noreply.github.com>
2022-06-10 21:52:03 +0530

    almost done now!!! 😄😇🥳

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?","/"];
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={,[,},],,,|,:,;,<,>,.?/";
 
 let buttonEl = document.getElementById("button")
 let fieldEl = document.getElementById("field")
+let fieldEl2 = document.getElementById("field2")
 
-function generate() {
+/*function generate() {
     for (let i = 0; i < characters.length; i++) {
         let randomPass1 = (Math.floor(Math.random() * 91))
 
@@ -13,4 +14,21 @@ function generate() {
 
         generate()
     }
+}
+
+*/
+
+function generate(length) {
+    let result = ' '
+    let result2 = ' '
+    const charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        result2 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    //return result
+
+    fieldEl.textContent = "Yor passwords are: " + result + " and " + result2
+
+    generate(15)
 }
